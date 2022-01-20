f5dc8e5ac794201f0fc74c9bb05841aff389078f
Alex Tzimas <atzimas@programize.com>
2022-01-20 18:34:15 +0200

    Create simple UI and simple classifier
    The classifier needs to be worked on
    because currently it detects cats as
    gyros too.
    
    I should consider increasing my dataset
    with random images of stuff that are
    not gyros.

--- gyros-identifier/gyros-identifier/ViewController.swift
+++ gyros-identifier/gyros-identifier/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    let imagePicker = UIImagePickerController()
+    let imageAlbumPicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage.")
+            }
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: GyrosClassifier_1().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {
+                (request, error) in
+                guard let results = request.results as? [VNClassificationObservation] else {
+                        fatalError("Model failed to process image.")
+                    }
+                if let firstResult = results.first {
+                    print(firstResult)
+                    if firstResult.identifier.contains("Gyros") {
+                        self.navigationItem.title = "Gyros ✅"
+                    } else {
+                        self.navigationItem.title = "Not Gyros ❌"
+                    }
+                }
+            }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
 
-
+    @IBAction func cameraAction(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func loadImageAction(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
 }
 
